5c252a66f615e8f6e98ef316efebce3e59f2c6ca
hxlnwpu <yishengtuwei@foxmail.com>
2020-11-20 09:40:07 +0800

    add 147

---
+++ Leetcode/147. 对链表进行插入排序.java
@@ -0,0 +1,27 @@
+class Solution {
+    public ListNode insertionSortList(ListNode head) {
+        if(head==null) return head;
+        ListNode dummyHead = new ListNode(-1);
+        dummyHead.next = head;
+        ListNode lastSorted = head;
+        ListNode curr = head.next;
+        while(curr!=null)
+        {
+            if(lastSorted.val<=curr.val)
+            {
+                lastSorted = lastSorted.next;
+            }
+            else
+            {
+                ListNode prev = dummyHead;
+                while(prev.next.val<=curr.val)
+                    prev = prev.next;
+                lastSorted.next = curr.next;
+                curr.next = prev.next;
+                prev.next = curr;
+            }
+            curr = lastSorted.next;
+        }
+        return dummyHead.next;
+    }
+}
